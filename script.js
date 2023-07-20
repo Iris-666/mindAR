d6999fb2c6218763ee8156416be8090fe35071cf
Iris <54792944+Iris-666@users.noreply.github.com>
2023-07-20 19:50:57 +0800

    photo update

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-import 'aframe';
-import 'mind-ar/dist/mindar-image-aframe.prod.js';
-import {MindARThree} from 'mind-ar/dist/mindar-image-three.prod.js';
+// import 'aframe';
+// import 'mind-ar/dist/mindar-image-aframe.prod.js';
+// import {MindARThree} from 'mind-ar/dist/mindar-image-three.prod.js';
+
+console.log("hello");
